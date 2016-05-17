c470aedbef702b3f92f8cc74fed252cd322a828e
Meg Walters <Meg.Walters@allstate.com>
2016-05-17 07:44:20 -0500

    Most of 4.6

--- fpinscala/errorhandling/Either.scala
+++ fpinscala/errorhandling/Either.scala
@@ -3,7 +3,24 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _}
 
 sealed trait Either[+E, +A]{
-    
+    def map[B](f: A => B): Either[E,B] = this match {
+        case Left(e) => Left(e)
+        case Right(a) => Right(f(a))
+    }
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+        case Left(e) => Left(e)
+        case Right(a) => f(a) 
+    }
+    def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+        case Left(e) => b
+        case Right(a) => Right(a)
+    }  
+    //def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = (this,b) match  {
+    //    case (Left(e), Left(f)) => f(this, b) 
+    //    case (Left(e), Right(a)) => f(this, a)
+    //    case (Right(a), Left(e)) => f(this, b)
+    //    case (Right(a), Right(b)) => f(this, b)
+    //} 
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
