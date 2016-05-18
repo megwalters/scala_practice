52f85ace5242dbf4f0b96fb9a34fd4972ebb8d40
Meg Walters <megwalterss@gmail.com>
2016-05-17 20:48:10 -0500

    Exercises 4.6 and 4.7

--- fpinscala/errorhandling/Either.scala
+++ fpinscala/errorhandling/Either.scala
@@ -3,24 +3,29 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _}
 
 sealed trait Either[+E, +A]{
+    //Exercise 4.6
     def map[B](f: A => B): Either[E,B] = this match {
         case Left(e) => Left(e)
         case Right(a) => Right(f(a))
     }
+
     def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
         case Left(e) => Left(e)
         case Right(a) => f(a) 
     }
+
     def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
         case Left(e) => b
         case Right(a) => Right(a)
     }  
-    //def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = (this,b) match  {
-    //    case (Left(e), Left(f)) => f(this, b) 
-    //    case (Left(e), Right(a)) => f(this, a)
-    //    case (Right(a), Left(e)) => f(this, b)
-    //    case (Right(a), Right(b)) => f(this, b)
-    //} 
+    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = (this,b) match  {
+    	case (Left(e1), Left(e2)) => Left(e1)
+    	case (Left(e), Right(a)) => Left(e) 
+    	case (Right(a), Left(e)) => Left(e)
+    	case (Right(a), Right(b)) => Right(f(a,b)) 
+    }
+
+    
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
@@ -37,5 +42,17 @@ object Either{
     def Try[A](a: => A): Either[Exception, A] = {
         try Right(a)
         catch {case e: Exception => Left(e)}
-    }    
+    }  
+	
+	//Exercise 4.7
+    def sequence[E, A](es: List[Either[E, A]]) : Either[E, List[A]] = es match {
+    	case Nil => Right(Nil)
+	case eh::et => eh.flatMap(h => sequence(et).map(h::_))
 }
+	
+
+	def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = as match {
+		case Nil => Right(Nil)
+		case ah::at => f(ah).map2(traverse(at)(f))(_::_)
+	}
+} 
